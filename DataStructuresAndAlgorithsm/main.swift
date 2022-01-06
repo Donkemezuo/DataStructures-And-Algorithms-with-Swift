a7afd1d73482970b1258ead63cb716beb5201ac8
Raymond Donkemezuo <raymonddonkemezuo@pursuit.org>
2022-01-05 19:31:55 -0600

    Nodes and Linked link implementation covered

--- DataStructuresAndAlgorithsm/main.swift
+++ DataStructuresAndAlgorithsm/main.swift
@@ -7,5 +7,182 @@
 
 import Foundation
 
-print("Hello, World!")
+/// Node class
+/*
+ Properties of a node
+ - Must have a value
+ - can have a reference to the next node on the linked list that it is pointing to (optional)
+ */
 
+class Node<Value> {
+    var value: Value
+    var next: Node?
+    init(value: Value, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+// To print out a node in the form of a linked list, we check if the node is pointing to another node. if yes, print the value of our node and print the next value as well
+extension Node: CustomStringConvertible {
+    var description: String {
+        // check if the current node is the last node
+        guard let nextNode = next else {
+            return "\(value)"
+        }
+        return "\(value) -> \(String(describing: nextNode)) "
+    }
+}
+
+let node1 = Node(value: 1)
+let node2 = Node(value: 2)
+let node3 = Node(value: 3)
+//node1.next = node2
+//node2.next = node3
+
+
+
+/// - TAG:  Linked List implementation
+/*
+ A linked list is a linear data structure which holds a series of nodes
+ 
+ - Our linked list is a generic type, so it can hold data of any time
+ 
+ Properties of a Linked list
+ - Head node (first node on the linked list )
+ - Tail node (last node on the linked list )
+ 
+ */
+
+struct LinkedList<Value> {
+    var head: Node<Value>?
+    var tail: Node<Value>?
+    init() {}
+}
+
+
+/*
+ Operations that can be performed on a linked list
+ 
+ - isEmpty: We can check if the linked list is empty. i.e if the head node is nil
+ - Push: Pushes a new node into the list. The pushed node will be the new head and the old head node will be the next node to the new head.
+ 
+ - Append: Adds a new node into the front of the linked list. i.e the appended node becomes the new tail of the linked list
+ - insert after: Given an index and a new value, we inside the new value after the given index
+ 
+ */
+
+extension LinkedList {
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    /// - TAG:  Adds a new head to the linked list
+    mutating func push(newValue: Value) {
+        if isEmpty {
+            head = Node(value: newValue)
+        } else {
+            let currentHeadNode = head
+            let newHeadNode = Node(value: newValue, next: currentHeadNode)
+            head = newHeadNode
+        }
+        
+        if tail == nil {
+            tail = head
+        }
+    }
+    /// - TAG:  Adds a new tail to the linked list
+    mutating func append(newValue: Value) {
+        if isEmpty {
+            push(newValue: newValue)
+        } else {
+            let currentTails = tail
+            let newTail = Node(value: newValue, next: nil)
+            tail = newTail
+            currentTails?.next = newTail
+        }
+        if head == nil {
+            head = tail
+        }
+    }
+    
+    /// - TAG: A function that returns a node given an index
+    func nodeAtIndex(at index: Int) -> Node<Value>? {
+        var currentIndex = 0
+        var currentNode = head
+        
+        while (currentNode != nil && currentIndex < index) {
+            currentNode = currentNode?.next
+            currentIndex += 1
+        }
+        return currentNode
+    }
+    
+    /// - TAG: Adds a new node to the linked list using a specific index
+    mutating func insertAfter(index: Int, newValue: Value) {
+        guard let nodeAtIndex = nodeAtIndex(at: index) else { return }
+        let newNode = Node(value: newValue, next: nodeAtIndex.next)
+        nodeAtIndex.next = newNode
+    }
+    
+    /// - TAG: Remove item from the link list. The pop operation removes nodes starting from the head
+    mutating func pop() -> Value? {
+        guard let currentHead = head else { return nil }
+        guard let newHead = currentHead.next else {
+            head = nil
+            tail = nil
+            return head?.value
+        }
+        
+        head = newHead
+        if isEmpty {
+            tail = nil
+        }
+        return currentHead.value
+    }
+    /// - TAG: Remove the current tail node from the linked list
+    mutating func removeLast() -> Value? {
+        guard !isEmpty else { return nil }
+        guard head?.next != nil else { return pop() }
+        
+        var current = head
+        var currentPrevious = head
+        
+        while (current?.next != nil ) {
+            currentPrevious = current
+            current = current?.next
+        }
+        currentPrevious?.next = nil
+        tail = currentPrevious
+        return current?.value
+    }
+    
+    mutating func removeAfter(index: Int) -> Value? {
+        guard !isEmpty else { return nil }
+        guard let nodeAtIndex = nodeAtIndex(at: index) else { return nil }
+        if nodeAtIndex.next === tail {
+            tail = nodeAtIndex
+        }
+        let deletingNode = nodeAtIndex.next
+        nodeAtIndex.next = deletingNode?.next
+        return deletingNode?.value
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    var description: String {
+        guard let head = head else { return " Linked list is empty " }
+        return head.description
+    }
+}
+
+var list = LinkedList<Int>()
+list.append(newValue: 2)
+list.append(newValue: 3)
+list.append(newValue: 22)
+list.append(newValue: 55)
+list.append(newValue: 64)
+list.insertAfter(index: 2, newValue: 80)
+print("Before removing: ", list)
+print("Removed after index 3", list.removeAfter(index: 3) ?? "No value after index 3")
+print("After removing: ", list)
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # DataStructures-And-Algorithms-with-Swift
+
+# Complexicity 
+/// - Complexity = is the amount of time and space (memory) needed to perform an algorithm 
+/*
+- There are different types of Time complexities 
+- Constant Time (O(1)) = A constant time algorithm is an algorithm that has the same time complexicity regardless of the size of the data the algorithm is working with 
+- Example of constant time operation, accessing a specific element in an array using it's index directly without iterating through the entire list 
+- Linear Time(O(N)) = A linear time complexicity is when the time needed to execute an algorithm depends directly on the size of the data. i.e the time increases as the data increases 
+- Example: Iterating through a list of items in an array and accessing each element. 
+- Quadratic Time(O(N^2)) = A quadratic time complexity happens in nested loops. Where the time it takes to complete the algorithm is the size of the data squared. 
+- Logarithmetic Time = A logarithmetic time complexity for algorithms that depends on only a fraction of the data size 
+*/
+
+# Linked List 
+/// - A linked list is a linear data structured which contains different elements.
+/// - Elements in a linked list are called nodes and are connected to each other through a reference 
+// Characteristics of a Linked List 
+    - The first node in a linked list is called the head. 
+    - The head node have not parent node
+    - The last node in a linked list is called the tail 
+    - The tail node have not child node 
